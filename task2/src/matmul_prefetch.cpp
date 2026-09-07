b84ee025dc29375b847413ef2fb61626e9e52fc4
keerthi <keerthiishwaryakollapu@gmail.com>
2026-09-07 06:52:51 +0000

    prefetch added

--- task2/src/matmul_prefetch.cpp
+++ task2/src/matmul_prefetch.cpp
@@ -3,10 +3,30 @@
 #include <immintrin.h>
 
 #include "matmul.h"
-
+#include <algorithm>
 void matmul_prefetch(const float* A, const float* B, float* C,
                      int M, int N, int K, int lda, int ldb, int ldc) {
     // TODO(student): replace this placeholder with your cache-blocked SIMD + prefetch
     // implementation.
-    matmul_naive(A, B, C, M, N, K, lda, ldb, ldc);
-}
+     for(int i=0;i<M;++i){
+        const float* a=A+i*lda;
+        _mm_prefetch((const char*)(A+(i+1)*lda),_MM_HINT_T2);
+        for(int j=0;j<N;++j){
+            const float* b=B+j*ldb;
+            _mm_prefetch((const char*)(B+(j+1*ldb)),_MM_HINT_T1);
+            float acc=0.0f;
+            for(int p=0;p<K;++p){
+                if((p&7)==0){
+               _mm_prefetch((const char*)&a[p+64],_MM_HINT_T1);
+               _mm_prefetch((const char*)&b[p+64],_MM_HINT_T1);
+                }
+                
+
+            
+            acc+=a[p]*b[p];
+            }
+            C[i*ldc+j]=acc;
+        }
+     }
+    }
+
